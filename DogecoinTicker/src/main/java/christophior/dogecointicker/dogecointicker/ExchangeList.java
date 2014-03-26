2d3552752ef30c653a486cfd7791609708937fc5
Zazador <zazador@gmail.com>
2014-03-25 22:09:20 -0500

    minor updates to notifications

--- DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/ExchangeList.java
+++ DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/ExchangeList.java
@@ -25,38 +25,9 @@ public class ExchangeList extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new ExchangeArrayAdapter(this, EXCHANGES));
-        createNotification();
     }
 
-    public void createNotification() {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_launcher)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, cryptsy.class);
 
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(cryptsy.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
-    }
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
--- DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Settings.java
+++ DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Settings.java
@@ -1,12 +1,22 @@
 package christophior.dogecointicker.dogecointicker;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
 
 /**
  * Created by Zach on 3/24/2014.
  */
-public class Settings extends PreferenceActivity {
+public class Settings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,63 @@ public class Settings extends PreferenceActivity {
         getPreferenceManager().setSharedPreferencesName("ttt_prefs");
         addPreferencesFromResource(R.xml.preferences);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+        //Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
+        createNotification(key);
+
+        if (pref instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) pref;
+            pref.setSummary(listPref.getEntry());
+            return;
+        }
+    }
+
+    public void createNotification(String key) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        //.setContentTitle("My notification")
+                        .setContentText(key + " 1 Doge = 999 Satoshi");
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, cryptsy.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(cryptsy.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
 
 }
