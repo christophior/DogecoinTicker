253d7ec5739914daceea64e2f75481b058da1822
Chris Villarreal <chrisvillarreal1018@gmail.com>
2014-03-26 20:22:22 -0500

    got some graph stuff working

--- DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/cryptsy.java
+++ DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/cryptsy.java
@@ -21,16 +21,19 @@ import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DecimalFormat;
+import java.util.*;
 
 
 public class cryptsy extends Activity {
 
     private static String urlCryptsy = "http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132";
-
+    protected static ArrayList<points> pointList = new ArrayList<points>();
+    ArrayList<GraphViewData> GraphData = new ArrayList<GraphViewData>();
     private ProgressDialog pDialog;
 
     @Override
@@ -46,37 +49,37 @@ public class cryptsy extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.cryptsy, menu);
 
-        System.out.println("creating graph");
-        // init series data
-        GraphViewSeries series = new GraphViewSeries(new GraphViewData[]{
-                new GraphViewData(1, 2.0d)
-                , new GraphViewData(2, 1.5d)
-                , new GraphViewData(3, 2.5d)
-                , new GraphViewData(4, 1.0d)}
-        );
-
-        GraphView graphView = new LineGraphView(
-                this // context
-                , "" // heading
-        );
-
-        graphView.addSeries(series); // data
-        // set styles
-//        graphView.getGraphViewStyle().setGridColor(Color.GREEN);
-//        graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.YELLOW);
-//        graphView.getGraphViewStyle().setVerticalLabelsColor(Color.RED);
-//        graphView.getGraphViewStyle().setTextSize(getResources().getDimension(R.dimen.big));
-//        graphView.getGraphViewStyle().setNumHorizontalLabels(5);
-//        graphView.getGraphViewStyle().setNumVerticalLabels(4);
-//        graphView.getGraphViewStyle().setVerticalLabelsWidth(300);
-        // set view port, start=2, size=40
-//        graphView.setViewPort(2, 40);
-//        graphView.setScrollable(true);
-        // optional - activate scaling / zooming
-//        graphView.setScalable(true);
-
-        LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
-        layout.addView(graphView);
+//        System.out.println("creating graph");
+//        // init series data
+//        GraphViewSeries series = new GraphViewSeries(new GraphViewData[]{
+//                new GraphViewData(1, 2.0)
+//                , new GraphViewData(2, 1.5)
+//                , new GraphViewData(3, 2.5)
+//                , new GraphViewData(4, 1.0)}
+//        );
+//
+//        GraphView graphView = new LineGraphView(
+//                this // context
+//                , "" // heading
+//        );
+//
+//        graphView.addSeries(series); // data
+//        // set styles
+////        graphView.getGraphViewStyle().setGridColor(Color.GREEN);
+////        graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.YELLOW);
+////        graphView.getGraphViewStyle().setVerticalLabelsColor(Color.RED);
+////        graphView.getGraphViewStyle().setTextSize(getResources().getDimension(R.dimen.big));
+////        graphView.getGraphViewStyle().setNumHorizontalLabels(5);
+////        graphView.getGraphViewStyle().setNumVerticalLabels(4);
+////        graphView.getGraphViewStyle().setVerticalLabelsWidth(300);
+//        // set view port, start=2, size=40
+////        graphView.setViewPort(2, 40);
+////        graphView.setScrollable(true);
+//        // optional - activate scaling / zooming
+////        graphView.setScalable(true);
+//
+//        LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
+//        layout.addView(graphView);
 
         return true;
     }
@@ -124,6 +127,18 @@ public class cryptsy extends Activity {
                     JSONObject dogeCryptsy = ((jsonObj.getJSONObject("return")).getJSONObject("markets")).getJSONObject("DOGE");
                     String cryptsyPrice = dogeCryptsy.getString("lasttradeprice");
                     System.out.println("Cryptsy Price: " + cryptsyPrice);
+                    JSONArray trades = (((jsonObj.getJSONObject("return")).getJSONObject("markets")).getJSONObject("DOGE")).getJSONArray("recenttrades");
+                    for (int i = trades.length()-1; i >= 0; i--){
+                        JSONObject t = trades.getJSONObject(i);
+                        pointList.add(new points(t.getString("time"), t.getDouble("price")));
+                        System.out.println("added trade(" + t.getString("id") + ") to list of points");
+                    }
+
+                    GraphData = new ArrayList<GraphViewData>();
+                    for (points p : pointList){
+                        GraphData.add(new GraphViewData(p.time, p.price));
+                    }
+
 //                    exchangePrices.put("Cryptsy", formatPricemBTC(cryptsyPrice));
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -144,10 +159,33 @@ public class cryptsy extends Activity {
 //            /**
 //             * Updating parsed JSON data into view
 //             * */
-//            "Cryptsy", "CoinedUp","Coins-E", "Bter", "Vircurex"
-//            double[] prices = {exchangePrices.get("Cryptsy"), exchangePrices.get("CoinedUp"),
-//                    exchangePrices.get("Coins-E"), exchangePrices.get("Bter"), exchangePrices.get("Vircurex")};
-//            setListAdapter(new ExchangeArrayAdapter(ExchangeList.this, EXCHANGES, prices));
+//
+            System.out.println("creating graph");
+            // init series data
+            GraphViewSeries series = new GraphViewSeries(GraphData.toArray(new GraphViewData[GraphData.size()]));
+
+            GraphView graphView = new LineGraphView(
+                    cryptsy.this // context
+                    , "" // heading
+            );
+
+            graphView.addSeries(series); // data
+            // set styles
+//        graphView.getGraphViewStyle().setGridColor(Color.GREEN);
+//        graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.YELLOW);
+//        graphView.getGraphViewStyle().setVerticalLabelsColor(Color.RED);
+//        graphView.getGraphViewStyle().setTextSize(getResources().getDimension(R.dimen.big));
+//        graphView.getGraphViewStyle().setNumHorizontalLabels(5);
+//        graphView.getGraphViewStyle().setNumVerticalLabels(4);
+//        graphView.getGraphViewStyle().setVerticalLabelsWidth(300);
+            // set view port, start=2, size=40
+//        graphView.setViewPort(2, 40);
+//        graphView.setScrollable(true);
+            // optional - activate scaling / zooming
+        graphView.setScalable(true);
+
+            LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
+            layout.addView(graphView);
         }
 
         public double formatPricemBTC(String price){
@@ -156,5 +194,26 @@ public class cryptsy extends Activity {
             String formattedPrice = df.format(result);
             return Double.parseDouble(formattedPrice);
         }
+    }
+}
+
+class points{
+    public String date;
+    public double price;
+    public double time;
+
+    public points(String d, double p){
+        date = d;
+        price = p;
+        time = generateTimeDouble(d);
+    }
+
+    public double generateTimeDouble(String d){
+        double result = 0;
+        String[] splitDate = d.split(":");
+        result = Double.parseDouble(splitDate[1]) + (Double.parseDouble(splitDate[2])/60);
+        System.out.println("SD1: " + splitDate[1] + " SD2: " + splitDate[2]);
+        System.out.println("Double rep: " + result);
+        return result;
     }
 }
