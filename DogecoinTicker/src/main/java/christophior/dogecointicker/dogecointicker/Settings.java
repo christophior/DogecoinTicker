6a9fc799b5e929e0568f944091ccb049acc8a93b
Zazador <zazador@gmail.com>
2014-04-28 00:06:32 -0500

    Notifications are getting there?
    Notifications correctly display all selected exchanges in only one
    notification, but notifications are still generated each time a new
    exchange is selected.

--- DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Settings.java
+++ DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Settings.java
@@ -18,9 +18,13 @@ import java.util.HashMap;
 public class Settings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private int mId = 0;
+    private int bigCheck = 0;
 
     private HashMap<String, Integer> notIDs = new HashMap<String, Integer>();
     private String currentCurrency = "mBTC";
+    NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(this);
+    NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +53,11 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
         ListPreference listPrefer = (ListPreference) findPreference("currency_to_use");
         CharSequence currTxt = listPrefer.getEntry();
         currentCurrency = listPrefer.getValue();
-        //Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
+//        if (bigCheck > 0) {
+//            inboxStyle.addLine("1 Doge = " + ExchangeList.exchangePrices.get(key)
+//                    + " " + currentCurrency + " @ " + key);
+//            Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
+//        }
         if (!key.equals("currency_to_use"))
             createNotification(key);
 
@@ -70,22 +78,26 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
             return;
         }
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_launcher)
-                        .setContentText(key + " \t\t1 Doge = " + ExchangeList.exchangePrices.get(key)
-                                + " " + currentCurrency)
-                        .setOngoing(true);
+
+//                        .setSmallIcon(R.drawable.ic_launcher)
+//                        .setContentText(key + " \t\t1 Doge = " + ExchangeList.exchangePrices.get(key)
+//                                + " " + currentCurrency)
+//                        .setOngoing(true);
         // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(this, Exchange.class);
 
-//        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-//        inboxStyle.setBigContentTitle("Event tracker details:");
-//        if (mId >= 1) {
-//            inboxStyle.addLine("1 Doge = " + ExchangeList.exchangePrices.get(key)
-//                    + " " + currentCurrency);
-//            mBuilder.setStyle(inboxStyle);
-//        }
+
+        inboxStyle.setBigContentTitle("Dogecoin Prices:");
+        //if (mId >= 1) {
+            inboxStyle.addLine("1 Doge = " + ExchangeList.exchangePrices.get(key)
+                    + " " + currentCurrency + " @ " + key);
+            if (bigCheck == 0) {
+                mBuilder.setStyle(inboxStyle);
+                mBuilder.setSmallIcon(R.drawable.ic_launcher);
+                mBuilder.setOngoing(true);
+            }
+        //}
+
 
         // The stack builder object will contain an artificial back stack for the
         // started Activity.
@@ -105,6 +117,7 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
         // mId allows you to update the notification later on.
         notIDs.put(key, mId);
         mId++;
+        bigCheck++;
         mNotificationManager.notify(notIDs.get(key), mBuilder.build());
     }
 }
