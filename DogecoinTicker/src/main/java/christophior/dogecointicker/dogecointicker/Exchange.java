ebd2beb5d2a410bd963adeac75005ce152ce6ed5
Chris Villarreal <chrisvillarreal1018@gmail.com>
2014-04-28 15:18:28 -0500

    messing around with axis labels and removed interval buttons

--- DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Exchange.java
+++ DogecoinTicker/src/main/java/christophior/dogecointicker/dogecointicker/Exchange.java
@@ -28,6 +28,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -37,6 +38,8 @@ public class Exchange extends Activity {
     ArrayList<GraphViewData> GraphData = new ArrayList<GraphViewData>();
     private ProgressDialog pDialog;
     public static double highPrice = -1.0, lowPrice = 9999.0, percentChange = 0.0;
+    boolean EveryOtherLine = true;
+    int startingHour, xAxisCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,12 @@ public class Exchange extends Activity {
         highPrice = -1.0;
         lowPrice = 9999.0;
         percentChange = 0.0;
+
+        EveryOtherLine = true;
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("HH");
+        startingHour = Integer.parseInt(df.format(c.getTime()))-2;
+        xAxisCount = 0;
 
         (new GetJson()).execute();
         setContentView(R.layout.activity_exchange);
@@ -108,8 +117,6 @@ public class Exchange extends Activity {
                     for (int i=0; i<trades.length(); ++i){
                         JSONArray t = trades.getJSONArray(i);
                         double time = t.getDouble(0), price = 1000*t.getDouble(1);
-                        time /= 10000;
-                        time -= 139800000;
                         GraphData.add(new GraphViewData(time, price));
 
                         // replace price with new format for high and low
@@ -119,7 +126,8 @@ public class Exchange extends Activity {
                         if (price < lowPrice)
                             lowPrice = price;
 
-                        System.out.println("added trade(" + time + ", " + price + ") to graph");
+//                        System.out.println("added trade(" + time + ", " + price + ") to graph");
+                        System.out.println(time);
                     }
                     double first = (trades.getJSONArray(0)).getDouble(1);
                     double last = (trades.getJSONArray(trades.length()-1)).getDouble(1);
@@ -156,10 +164,34 @@ public class Exchange extends Activity {
 
             // init series data
             GraphViewSeries series = new GraphViewSeries(GraphData.toArray(new GraphViewData[GraphData.size()]) );
-            GraphView graphView = new LineGraphView(
-                    Exchange.this // context
-                    , "" // heading
-            );
+            GraphView graphView = new LineGraphView(Exchange.this, "");
+
+            graphView.setCustomLabelFormatter(new CustomLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+
+                    if (isValueX) {
+                        if (EveryOtherLine){
+                            EveryOtherLine = false;
+                            int hourVal = (startingHour + xAxisCount) % 24;
+                            xAxisCount+=2;
+                            return hourVal + ":00";
+                        } else {
+                            EveryOtherLine = true;
+                            xAxisCount+=2;
+                            return "";
+                        }
+                    } else {
+                        if (EveryOtherLine){
+                            EveryOtherLine = false;
+                            return null;
+                        } else {
+                            EveryOtherLine = true;
+                            return "";
+                        }
+                    }
+                }
+            });
 
             graphView.addSeries(series); // data
             // set styles
@@ -180,7 +212,7 @@ public class Exchange extends Activity {
 //              graphView.getGraphViewStyle().setHorizontalLabelsColor(Color.YELLOW);
 //              graphView.getGraphViewStyle().setVerticalLabelsColor(Color.RED);
 //                graphView.getGraphViewStyle().setTextSize(0);
-                graphView.getGraphViewStyle().setNumHorizontalLabels(12);
+                graphView.getGraphViewStyle().setNumHorizontalLabels(13);
                 graphView.getGraphViewStyle().setNumVerticalLabels(15);
 //                graphView.getGraphViewStyle().setVerticalLabelsWidth(0);
 //              graphView.setViewPort(2, 40);
