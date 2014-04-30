7808de71a7c36b77a6b7d0f898c600fb1a804fcc
Zazador <zazador@gmail.com>
2014-04-29 22:10:53 -0500

    Ongoing Notifications Update Correctly

--- app/src/main/java/christophior/dogecointicker/app/NotificationReceiver.java
+++ app/src/main/java/christophior/dogecointicker/app/NotificationReceiver.java
@@ -8,21 +8,32 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
 import static christophior.dogecointicker.app.Settings.share;
 
 public class NotificationReceiver extends BroadcastReceiver{
 
     public static String UPDATE_NOTI = "christophior.dogecointicker.app.UPDATE_NOTI";
+    public static HashMap<String, Double> exchangePrices = new HashMap<String, Double>();
+    private static String urlMarketPrices = "http://doge.yottabyte.nu/json/markets.json";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.v("updatetest", "please update");
+        (new GetJson()).execute();
 
         SharedPreferences prefs = share;
 
@@ -55,19 +66,19 @@ public class NotificationReceiver extends BroadcastReceiver{
         }
 
         if (prefs.getBoolean(("Cryptsy"), false))
-            inboxStyle.addLine("1 Doge = " + MainActivity.exchangePrices.get("Cryptsy")
+            inboxStyle.addLine("1 Doge = " + exchangePrices.get("Cryptsy")
                     + " mBTC @ Cryptsy");
         if (prefs.getBoolean(("CoinedUp"), false))
-            inboxStyle.addLine("1 Doge = " + MainActivity.exchangePrices.get("CoinedUp")
+            inboxStyle.addLine("1 Doge = " + exchangePrices.get("CoinedUp")
                     + " mBTC @ CoinedUp");
         if (prefs.getBoolean(("Coins-E"), false))
-            inboxStyle.addLine("1 Doge = " + MainActivity.exchangePrices.get("Coins-E")
+            inboxStyle.addLine("1 Doge = " + exchangePrices.get("Coins-E")
                     + " mBTC @ Coins-E");
         if (prefs.getBoolean(("Bter"), false))
-            inboxStyle.addLine("1 Doge = " + MainActivity.exchangePrices.get("Bter")
+            inboxStyle.addLine("1 Doge = " + exchangePrices.get("Bter")
                     + " mBTC @ Bter");
         if (prefs.getBoolean(("Vircurex"), false))
-            inboxStyle.addLine("1 Doge = " + MainActivity.exchangePrices.get("Vircurex")
+            inboxStyle.addLine("1 Doge = " + exchangePrices.get("Vircurex")
                     + " mBTC @ Vircurex");
 
         // The stack builder object will contain an artificial back stack for the
@@ -88,5 +99,79 @@ public class NotificationReceiver extends BroadcastReceiver{
         // mId allows you to update the notification later on.
 
         mNotificationManager.notify(2, mBuilder.build());
+    }
+
+
+    /**
+     * Async task class to get json by making HTTP call
+     * */
+    private class GetJson extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        private void updateExchangePrice(String exchange, double price){
+
+            int coefficient = 1000;
+
+            if (exchange.equals("cryptsy")){
+                exchangePrices.put("Cryptsy", formatPricemBTC(price));
+            } else if (exchange.equals("coinedup")){
+                exchangePrices.put("CoinedUp", formatPricemBTC(price));
+            } else if (exchange.equals("coins-e")){
+                exchangePrices.put("Coins-E", formatPricemBTC(price));
+            } else if (exchange.equals("bter")){
+                exchangePrices.put("Bter", formatPricemBTC(price));
+            } else if (exchange.equals("vircurex")){
+                exchangePrices.put("Vircurex", formatPricemBTC(price));
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonMarketPrice = sh.makeServiceCall(urlMarketPrices, ServiceHandler.GET);
+
+            if (jsonMarketPrice != null) {
+                try {
+                    JSONArray jsonArray = new JSONArray(jsonMarketPrice);
+
+                    for (int i=0; i<jsonArray.length(); i++){
+                        JSONObject singleMarket = jsonArray.getJSONObject(i);
+                        updateExchangePrice(singleMarket.getString("m"), singleMarket.getDouble("l"));
+                    }
+
+                    System.out.println("Cryptsy Price: " + exchangePrices.get("Cryptsy"));
+                    System.out.println("CoinedUp Price: " + exchangePrices.get("CoinedUp"));
+                    System.out.println("Coins-E Price: " + exchangePrices.get("Coins-E"));
+                    System.out.println("Bter Price: " + exchangePrices.get("Bter"));
+                    System.out.println("Vircurex Price: " + exchangePrices.get("Vircurex"));
+
+                } catch (JSONException e) { e.printStackTrace(); }
+
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            double[] prices = {exchangePrices.get("Cryptsy"), exchangePrices.get("CoinedUp"),
+                    exchangePrices.get("Coins-E"), exchangePrices.get("Bter"), exchangePrices.get("Vircurex")};
+        }
+
+        public double formatPricemBTC(Double price){
+            DecimalFormat df = new DecimalFormat("0.00000");
+            double result = price * 1000;
+            String formattedPrice = df.format(result);
+            return Double.parseDouble(formattedPrice);
+        }
     }
 }
